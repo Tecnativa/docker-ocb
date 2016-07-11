44b79418747b537a91925f419749b408d6b6fa54
Jairo Llopis <yajo.sk8@gmail.com>
2016-07-11 18:26:20 +0200

    Add required PRs to build.

--- hooks/build.sh
+++ hooks/build.sh
@@ -10,4 +10,14 @@ if [ -z "$version" -o "$version" == "latest" ]; then
     version=9.0
 fi
 
+# Required PRs
+cd ./addons/available/OCA/OCB
+git remote add tecnativa-odoo https://github.com/Tecnativa/odoo.git
+git fetch
+git merge --no-edit \
+    9.0-module-addons_path_len_reversed \
+    9.0-translate-choose_right_path_with_similar_prefixes
+
+# Now build it
+cd ../../../..
 docker build --build-arg ODOO_VERSION=$version -t $IMAGE_NAME .
