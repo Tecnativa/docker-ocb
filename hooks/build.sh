9bf1a281d5eb319618a0c4e311eb6b213d15a4ec
Jairo Llopis <yajo.sk8@gmail.com>
2016-09-23 12:55:29 +0200

    Update and merge https://github.com/OCA/account-financial-reporting/pull/229.

--- hooks/build.sh
+++ hooks/build.sh
@@ -2,6 +2,7 @@
 # See https://github.com/docker/hub-feedback/issues/508#issuecomment-222520720.
 set -ex
 IMAGE_NAME=${IMAGE_NAME:-tecnativa/ocb:latest}
+DIR="$(pwd)"
 
 # Get only version name
 version="$(echo $IMAGE_NAME | sed 's/.*://')"
@@ -14,5 +15,10 @@ fi
 git config --global user.name "Tecnativa's Docker Hub Bot"
 git config --global user.email info@tecnativa.com
 
+# Required PRs
+cd $DIR/addons/available/OCA/account-financial-reporting
+git fetch origin pull/229/head:pr229
+git merge --no-edit pr229
+
 # Now build it
-docker build --build-arg ODOO_VERSION=$version -t $IMAGE_NAME .
+docker build --build-arg ODOO_VERSION=$version -t $IMAGE_NAME "$DIR"
